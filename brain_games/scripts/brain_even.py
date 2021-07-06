9fd8290716b8fec0593a551df63d12fac78d50de
Andrey Bolgartsev <bolgartsevav@gmail.com>
2021-07-06 22:07:02 +0300

    update all games

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,9 +3,9 @@ from brain_games.games.even import even_up, introduction_question
 from brain_games.engine_brain import count_algo_check
 
 
-
 def main():
-    count_algo_check(even_up, introduction_question)  # передаем аргумент как функцию
+    # передаем аргумент как функцию
+    count_algo_check(even_up, introduction_question)
 
 
 if __name__ == '__main__':
