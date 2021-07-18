a05007ce71da17b22b3bf0f8b1963379a56ddb9b
Andrey Bolgartsev <bolgartsevav@gmail.com>
2021-07-18 13:21:50 +0300

    update games

--- brain_games/games/greatest_divisor_gcd.py
+++ brain_games/games/greatest_divisor_gcd.py
@@ -10,15 +10,10 @@ def introduction_to_game():
     return 'Find the greatest common divisor of given numbers.'
 
 
-def random_number():
+def check_gcd():
     number_1 = random.randint(START_INTERVAL, END_INTERVAL)
     number_2 = random.randint(START_INTERVAL, END_INTERVAL)
     game_question = f'{number_1} {number_2}'
-    return game_question
-
-
-def check_gcd():
-    game_question = random_number()
     while number_2 > 0:
         number_1, number_2 = number_2, number_1 % number_2
     return game_question, number_1
